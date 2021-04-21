e8c389886158310bfbc37e17bd7e6a2248b46c58
Dictator <aouvarov8@gmail.com>
2021-04-21 18:29:01 +0300

    DA

--- Assets/Scripts/CameraAnimator.cs
+++ Assets/Scripts/CameraAnimator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraAnimator : MonoBehaviour
 {
 
-    [SerializeField] Animator animator; 
+    [SerializeField] Animator animator;
+    [SerializeField] Level level;
 
     public void SetGameOverTrigger()
     {
@@ -14,5 +15,25 @@ public class CameraAnimator : MonoBehaviour
     public void SetLevelCompleteTrigger()
     {
         animator.SetTrigger("LevelComplete");
+    }
+
+    public void LoadLevelCompleteScene()
+    {
+        int nextSceneIndex = PlayerPrefs.GetInt(Level.NEXT_LEVEL_KEY, Level.LEVEL_TO_START);
+
+        if (nextSceneIndex == level.GetTotalSceneNumber() + 1)
+        {
+            PlayerPrefs.SetInt(Level.GAME_COMPLETE_KEY, Level.GAME_COMPLETE);
+            level.LoadLevelSelectionScene();
+        }
+        else
+        {
+            level.LoadLevelCompleteScene();
+        }
+    }
+
+    public void LoadGameOverScene()
+    {
+        level.LoadLevelFailScene();
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    public static string NEXT_LEVEL_KEY = "nextLevelToPlay";
+    
+    public static int LEVEL_TO_START = 1;
+
+    public static string LAST_LEVEL_KEY = "lastPlayedLevel";
+
+    public static int LEVEL_TO_PLAY = 1;
+
+    public static string GAME_COMPLETE_KEY = "gameComplete";
+
+    public static int GAME_INCOMPLETE = 0;
+    public static int GAME_COMPLETE = 1;
+
+
+    [SerializeField] Button[] levelButtons;
+    [SerializeField] int totalSceneNumber = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EnableLevelLoadButtons()
+    {
+        if(levelButtons.Length == 0)
+        {
+            return;
+        }
+        
+    }
+
+    public int GetTotalSceneNumber()
+    {
+        return totalSceneNumber;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void LoadLevelCompleteScene()
+    {
+        SceneManager.LoadScene("LevelComplete");
+    }
+    public void LoadLevelFailScene()
+    {
+        SceneManager.LoadScene("LevelFail");
+    }
+
+    public void LoadLevelSelectionScene()
+    {
+        SceneManager.LoadScene("SelectLevel");
+    }
+
+}

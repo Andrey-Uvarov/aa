1ae3bb3a283cea373999b60de27e829748d8698d
Dictator <aouvarov8@gmail.com>
2021-04-30 16:41:36 +0300

    Completely implement level load and level progress system

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -20,9 +20,10 @@ public class Manager : MonoBehaviour
         currentNumberOfPins++;
     }
 
-    public void GameOver()
+    public void GameOver(int sceneIndex)
     {
         DisableGameElements();
+        PlayerPrefs.SetInt(Level.LAST_LEVEL_KEY, sceneIndex);
         animator.SetGameOverTrigger();
     }
 
